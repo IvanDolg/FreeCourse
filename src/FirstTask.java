56f6a5f11cc0eb4a237c1c4600d3b2c5a261ac7d
Ivan Dauhalaptseu <ivandolgolaptev@gmail.com>
2023-06-01 00:27:47 +0300

    feat: add 4 task from loop

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class FirstTask {
     public static void main(String[] args) {
         scanner();
-        getNnmber();
-        getSquaresNum();
-        getSquaresMultiplication();
+        //getNnmber();
+        //getSquaresNum();
+        //getSquaresMultiplication();
+        getSum();
     }
 
     public static Scanner scanner(){
@@ -53,5 +54,30 @@ public class FirstTask {
             multiplication *= sum;
         }
         System.out.println("Sum of the squares multiplication from 1 to " + num + " is " + multiplication);
+    }
+    // 4 task
+    // Enter the number E. Find the sum of those terms of the series whose modulus is greater than or equal
+    // to the given e.
+    public static void getSum(){
+        System.out.println("\n\t\t ---- 4 TASK ----");
+        // calculation of the common member
+        double a = 0;
+        double sum = 0;
+        double n = 1;
+
+        System.out.print("Enter e num: ");
+        double e = scanner().nextDouble();
+
+
+        for (int i = 1; i <= n; i++){
+            a = (1/Math.pow(2, n)) + (1/Math.pow(3, n));
+            if (Math.abs(a) >= e){
+                sum += a;
+                n++;
+            } else sum = 0;
+        }
+        System.out.println("General element: " + a);
+        System.out.println("Number e: " + e);
+        System.out.println("Sum is: " + sum);
     }
 }
